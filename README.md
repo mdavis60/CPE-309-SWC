7b57994bd14082d4d944a4e6fe89170850ff8b6d
jackson darrow <jackson.darrow@gmail.com>
2017-02-01 10:22:12 -0800

    added dependencies for testing and database orm

--- README.md
+++
@@ -1,2 +0,0 @@
-# CPE-309-SWC
-Repository for the code and documents used by SWC group in CPE 309 class.
---
+++ src/test/java/org/swp/scheduler/DBUnitTests.java
@@ -0,0 +1,18 @@
+package org.swp.scheduler;
+import org.testng.annotations.*;
+
+/**
+ * Created by jackson on 2/1/17.
+ */
+public class DBUnitTests {
+    @BeforeClass
+    public void setup() {
+
+    }
+
+    @Test
+    public void test() {
+        assert true;
+    }
+
+}
