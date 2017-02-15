dea511f649ca8b73d35459c570f3e4b5bb94b4d2
Michael McAniff <michael.mcaniff@gmail.com>
2017-02-15 13:34:31 -0800

    Set up the tableview

--- src/main/java/org/swp/scheduler/SchedulerController.java
+++ src/main/java/org/swp/scheduler/SchedulerController.java
@@ -13,6 +13,8 @@ import java.util.ArrayList;
 import org.slf4j.*;
 import org.swp.scheduler.database.models.*;
 
+import javafx.collections.transformation.*;
+
 public class SchedulerController extends WindowController {
 
 
@@ -23,16 +25,8 @@ public class SchedulerController extends WindowController {
 	private Group studentFeedback;
 
 	@FXML
-	private AnchorPane rightPane;
-
-	@FXML
-	private AnchorPane leftPane;
-
-	@FXML
-	private SplitPane splitPane;
-
-	@FXML
 	private Group sidePanel;
+	
 	@FXML
 	private AnchorPane calendarAnchor;
 	
@@ -46,17 +40,66 @@ public class SchedulerController extends WindowController {
 	private Group calendarGroup;
 
 	@FXML 
-	private HBox listHBox;
+	private TableView<Section> listTable;
 	
 	@FXML
 	private Group innercalendarGroup; 
 	
-
 	@FXML 
 	private HBox calendarHBox; 
 	
+	@FXML
+	private TableColumn<Section, String> sectionNameColumn;
+	
+	@FXML
+	private TableColumn<Section, String> profColumn;
+	
+	@FXML
+	private TableColumn<Section, String> roomColumn;
+	
+	@FXML
+	private TableColumn<Section, String> sectionTypeColumn;
+	
+	@FXML
+	private TableColumn<Section, String> timeColumn;
+	
 	private boolean feedbackIsShown = true; 
+	
+	
+    private ObservableList<Section> masterData = FXCollections.observableArrayList();
+    
+    public SchedulerController() {
+    	masterData.add(new Section("CPE 309", "T. Kearns", "14-202", "Lecture", "M W F", "9", "11"));
+    	masterData.add(new Section("CPE 308", "T. Kearns", "14-202", "Lecture", "M W F", "1", "3"));
+    	masterData.add(new Section("CPE 305", "T. Kearns", "14-202", "Lecture", "M W F", "12", "1"));
+    	masterData.add(new Section("CPE 357", "T. Kearns", "14-202", "Lecture", "M W F", "8", "9"));
+    	masterData.add(new Section("CPE 101", "T. Kearns", "14-202", "Lecture", "M W F", "3", "5"));
+    	masterData.add(new Section("CPE 102", "T. Kearns", "14-202", "Lecture", "M W F", "5", "7"));
+    }
+    
+    @FXML
+    private void initialize() {
+    	System.out.println("Top");
+		sectionNameColumn.setCellValueFactory(cellData -> cellData.getValue().courseProperty());
+		profColumn.setCellValueFactory(cellData -> cellData.getValue().profProperty());
+		roomColumn.setCellValueFactory(cellData -> cellData.getValue().roomProperty());
+		sectionTypeColumn.setCellValueFactory(cellData -> cellData.getValue().courseCompProperty());
+		timeColumn.setCellValueFactory(cellData -> cellData.getValue().startTimeProperty());
 
+
+        // 1. Wrap the ObservableList in a FilteredList (initially display all data).
+        FilteredList<Section> filteredData = new FilteredList<>(masterData, p -> true);
+
+        // 3. Wrap the FilteredList in a SortedList. 
+        SortedList<Section> sortedData = new SortedList<>(filteredData);
+
+        // 4. Bind the SortedList comparator to the TableView comparator.
+        sortedData.comparatorProperty().bind(listTable.comparatorProperty());
+
+        // 5. Add sorted (and filtered) data to the table.
+        listTable.setItems(sortedData);
+        System.out.println("Added Sorted Data");
+    }
 	
     public void start(Stage stage) throws Exception {
     	System.out.println("Started");
@@ -67,8 +110,6 @@ public class SchedulerController extends WindowController {
     	System.out.println("New Course");
     	openWindow("CreateCourse");
     }
-
-    
 
     @FXML
     void launchNewSection() {
@@ -121,78 +162,22 @@ public class SchedulerController extends WindowController {
 	}
 	
 	public void onAddSection() throws Exception {
-		Course course = new Course(); 
-		Teacher prof = new Teacher();
-		Room room = new Room(); 
-		CourseComponent comp = new CourseComponent(); 
-		
-		course.courseName = "CPE 309";
-		comp.type = "Lecture";
-		prof.teacherName = "T. Kearns";
-		room.building = "14";
-		room.roomNumber = 255; 
-		
-		Section section = new Section();
-		section.course = course; 
-		section.courseComp = comp; 
-		section.prof = prof; 
-		section.room = room;
-		section.startTime = 9; 
-		section.endTime = 11; 
-		section.dow = "M W F";
-	
-		addSection(section);
+    	masterData.add(new Section("CPE 309", "M. McAniff", "14-202", "Lecture", "M W F", "9", "11"));
 	} 
 	
+	
+	
 	public void addSection(Section section) throws Exception {
-		addSectionToListView(section);
-		addSectionToCalendarView(section);
+		
 	}
 
+	
 	public void addSectionToCalendarView(Section section) throws Exception {
-		Text name = new Text(section.course.courseName);
-		Text prof = new Text(section.prof.teacherName);
-		Text hours = new Text(section.startTime + "-" + section.endTime);
-		
-		
-		ObservableList<Node> list = calendarHBox.getChildren(); 
-		
-		AnchorPane ap = new AnchorPane();
-		VBox v = new VBox();
-		ap.getStyleClass().add("calendar-block");
-		ap.setPrefHeight(100);
-		ap.getChildren().add(v);
-		v.getChildren().add(name);
-		v.getChildren().add(prof);
-		v.getChildren().add(hours);
-		
-		
-		//for (int i = 0; i < list.size(); i++) {
-			VBox vb = (VBox) list.get(0);
-			vb.getChildren().add(ap);
-	//	}
-		
-		
+
 	}	
 	
 	public void addSectionToListView(Section section) throws Exception {
-		ArrayList<String> sectionInfo = new ArrayList<String>();
-		sectionInfo.add(section.course.courseName);
-		sectionInfo.add(String.valueOf(section.dow + " : " + section.startTime) + " - " + String.valueOf(section.endTime));
-		sectionInfo.add(section.prof.teacherName);
-		sectionInfo.add(section.room.getRoom());
-		sectionInfo.add(section.courseComp.type);
+
 		
-		ObservableList<Node> list = listHBox.getChildren(); 
-		
-		for (int i = 0; i < list.size(); i++) {
-			VBox vb = (VBox) list.get(i);
-			Text text = new Text(sectionInfo.get(i));
-			text.setTextAlignment(TextAlignment.CENTER);
-			vb.getChildren().add(text);
-		}
 	}
-	
-	
-	
 }
