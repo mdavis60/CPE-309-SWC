b6713a84ca211193e7fd3fa3e7bd35a37c6a4e86
Robert Weber <RAWeber95@gmail.com>
2017-02-16 22:23:36 -0800

    Storing student feedback in database

--- src/main/java/org/swp/scheduler/StudentFeedbackController.java
+++ src/main/java/org/swp/scheduler/StudentFeedbackController.java
@@ -1,5 +1,10 @@
 package org.swp.scheduler;
 
+import org.swp.scheduler.database.DatabaseException;
+import org.swp.scheduler.database.DatabaseManager;
+import org.swp.scheduler.database.models.LoginData;
+import org.swp.scheduler.database.models.StudentFeedback;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;	
 import javafx.scene.control.TextArea;
@@ -13,18 +18,21 @@ public class StudentFeedbackController extends WindowController {
     @FXML private Button cancelButton;
     
     //function to submit student feedback
-    public void submitFeedback() {
+    public void submitFeedback() throws DatabaseException {
     	
     	String subject = subjectField.getText();
     	String message = messageField.getText();
 
-        subjectField.setText("Test");
-        messageField.setText("This is a test");
+    	if(!subject.isEmpty() && !message.isEmpty()){
+    	     StudentFeedback feedback = new StudentFeedback(message, subject);
+    	     DatabaseManager.getInstance().storeSingle(feedback);
+    	}
     }
     
     @FXML
-    void submitPressed() {
+    void submitPressed() throws DatabaseException {
     	System.out.println("Submit button was pressed!");
+    	submitFeedback();
     	closeWindow(submitButton);
     }
     
