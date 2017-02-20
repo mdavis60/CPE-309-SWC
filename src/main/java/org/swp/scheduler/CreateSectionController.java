0828d9013418331477fa06c4acf6f0cd15289be7
mdavis60 <mitchelcdavis@gmail.com>
2017-02-20 15:03:19 -0800

    Added functionality to Create Section Page

--- src/main/java/org/swp/scheduler/CreateSectionController.java
+++ src/main/java/org/swp/scheduler/CreateSectionController.java
@@ -1,7 +1,10 @@
 package org.swp.scheduler;
 
+import org.swp.scheduler.database.models.Section;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
@@ -21,7 +24,19 @@ public class CreateSectionController extends WindowController {
     private TextField room;
 
     @FXML
-    private ToggleGroup WeekDay;
+    private RadioButton mondayToggle;
+
+    @FXML
+    private RadioButton tuesdayToggle;
+
+    @FXML
+    private RadioButton wednesdayToggle;
+
+    @FXML
+    private RadioButton thursdayToggle;
+
+    @FXML
+    private RadioButton fridayToggle;
 
     @FXML
     private TextField startTime;
@@ -43,6 +58,39 @@ public class CreateSectionController extends WindowController {
     @FXML
     void CreateSection() {
       System.out.println("Section Created");
+      String course = courseType.getText();
+      String prof = professor.getText();
+      String roomNum = room.getText();
+      String start = startTime.getText();
+      String end = endTime.getText();
+      
+      MasterController.getInstance().addToData(new Section(course, prof , roomNum, "Lecture", getDaysOfWeek(), start, end));
       closeWindow(createButton);
+    }
+    
+    private String getDaysOfWeek()
+    {
+    	String days = "";
+    	if(mondayToggle.isSelected())
+    	{
+    		days += "M ";
+    	}
+    	if(tuesdayToggle.isSelected())
+    	{
+    		days += "T ";
+    	}
+    	if(wednesdayToggle.isSelected())
+    	{
+    		days += "W ";
+    	}
+    	if(thursdayToggle.isSelected())
+    	{
+    		days += "R ";
+    	}
+    	if(fridayToggle.isSelected())
+    	{
+    		days += "F ";
+    	}
+    	return days.trim();
     }
 }
