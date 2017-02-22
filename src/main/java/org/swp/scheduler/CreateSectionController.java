9b962d48cec00caaa0357e60c3bcb38bc80b9c8e
Michael McAniff <michael.mcaniff@gmail.com>
2017-02-22 12:22:04 -0800

    Create Section changes

--- src/main/java/org/swp/scheduler/CreateSectionController.java
+++ src/main/java/org/swp/scheduler/CreateSectionController.java
@@ -1,60 +1,40 @@
 package org.swp.scheduler;
 
-import org.swp.scheduler.database.models.Section;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.stage.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.*;
+import javafx.scene.shape.*;
+import javafx.application.*;
+import javafx.collections.*;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
 
+import java.util.ArrayList;
+
+import org.slf4j.*;
+import org.swp.scheduler.database.models.*;
+
+import javafx.collections.transformation.*;
+import javafx.util.Callback;
 public class CreateSectionController extends WindowController {
 
-    @FXML
-    private TextField courseType;
-
-    @FXML
-    private ListView<?> courseTypeList;
-
-    @FXML
-    private TextField professor;
-
-    @FXML
-    private TextField room;
-
-    @FXML
-    private RadioButton mondayToggle;
-
-    @FXML
-    private RadioButton tuesdayToggle;
-
-    @FXML
-    private RadioButton wednesdayToggle;
-
-    @FXML
-    private RadioButton thursdayToggle;
-
-    @FXML
-    private RadioButton fridayToggle;
-
-    @FXML
-    private TextField startTime;
-
-    @FXML
-    private TextField endTime;
-
-    @FXML
-    private Button createButton;
+ @FXML
+ private Text addComponent; 
+ 
+ @FXML
+ private AnchorPane mainAnchor;
     
-    public void OpenList(){
-      courseTypeList.setVisible(true);
-    }
+ 	@FXML
+ 	public void onAddComponent() {
+ 		System.out.println("Adding component");
+ 		mainAnchor.setPrefHeight(mainAnchor.getHeight() * 2);
+ 	}
     
-    public void CloseList(){
-      courseTypeList.setVisible(false);
-    }
-
+    /*
     @FXML
     void CreateSection() {
       System.out.println("Section Created");
@@ -92,5 +72,5 @@ public class CreateSectionController extends WindowController {
     		days += "F ";
     	}
     	return days.trim();
-    }
+    }*/
 }
