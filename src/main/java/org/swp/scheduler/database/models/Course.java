80f41e7a7b854cc58dcd462a9c080c3e50a54613
mdavis60 <mitchelcdavis@gmail.com>
2017-02-22 09:51:30 -0800

    course datamodel

--- src/main/java/org/swp/scheduler/database/models/Course.java
+++ src/main/java/org/swp/scheduler/database/models/Course.java
@@ -12,7 +12,6 @@ import java.util.List;
 @Table(name = "Courses")
 public class Course extends Model {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
     public int courseId;
     public String department;
     public String courseName;
@@ -21,20 +20,18 @@ public class Course extends Model {
 
     //https://stackoverflow.com/questions/18379766/hql-hibernate-inner-join
     //@OneToMany(mappedBy="employee",cascade=CascadeType.ALL)
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinColumn(name="courseId")
+    @OneToMany
+    @JoinColumn(name="courseComponentId")
     public List<CourseComponent> componentList;
 
     //courseName, courseNumber, courseType, prereqs, workUnits, studentUnits
     public Course() {
     }
 
-    public Course(int courseNumber, String courseName, String prerequisites, String department) {
-        this.courseNumber = courseNumber;
-        this.courseName = courseName;
+    public Course(int courseId, String courseName, String department, List<CourseComponent> components) {
+        this.courseId = courseId;
         this.department = department;
         this.courseName = courseName;
-        this.prerequisites = prerequisites;
     }
     public String getCourseName()
     {
