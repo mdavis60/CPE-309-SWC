dd8872efb741e00bfb59963f710c99b22e1dba47
mdavis60 <mitchelcdavis@gmail.com>
2017-02-24 10:44:40 -0800

    My Changes

---
+++ src/main/java/org/swp/scheduler/CalendarSectionBlock.java
@@ -0,0 +1,38 @@
+package org.swp.scheduler;
+
+import javafx.fxml.FXML;
+
+import org.swp.scheduler.database.models.Section;
+
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Label;
+
+public class CalendarSectionBlock extends AnchorPane{
+
+    @FXML
+    private Label course;
+
+    @FXML
+    private Label sectionNumber;
+
+    @FXML
+    private Label startTime;
+
+    @FXML
+    private Label endTime;
+
+    @FXML
+    private Label professor;
+
+    @FXML
+    private Label room;
+    
+	public CalendarSectionBlock(Section section, int sectionNumber) {
+		course.setText(section.getCourse());
+		this.sectionNumber.setText("" + sectionNumber);
+		professor.setText(section.getProf());
+		room.setText(section.getRoom());
+		startTime.setText(section.getStartTime());
+		endTime.setText(section.getEndTime());
+	}
+}
